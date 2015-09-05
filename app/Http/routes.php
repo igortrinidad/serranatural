a89a9f68debfa11b8328f2a423b4521aa4fa3ce1
Igor Trindade <igorlucast>
2015-09-05 16:16:53 -0300

    Melhorias

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -35,6 +35,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/teste', function () {
+    return view('emails/password');
+});
+
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,19 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Recuperar a senha</title>
-</head>
-<body>
+@extends('layout/emailBasico')
+
+@section('conteudo')
+
+<style>
+body{
+	background-color: grey;
+}
+.panel{
+margin: auto;
+padding-right: 20px;
+padding-left: 20px;
+margin-top: 30px;
+margin-bottom: 30px;
+}
+</style>
+
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<div class="panel">
+<div class="panel-heading text-center">
+	<img src="https://lh5.googleusercontent.com/-EgbVX_pkBVc/VUfx53ls4-I/AAAAAAAAADc/7ny4hjr_G7s/s203-k-no/" />
+</div>
+<div class="panel-body">
 <h3>E-mail de recuperação de senha</h3>
 
 <p>Recebemos a sua solicitação de senha.</p>
 
 <p>Utilize a nova senha para logar no sistema.</p>
 
-<p><strong>Nova senha: {{$pass}}</strong></p>
+<p><strong>Nova senha: <span class="btn btn-default btn-block">{{isset($pass) ? $pass : ''}}</span></strong></p>
 
-<p>Lembranças,<p>
+<p>Lembranças,</p>
 <p>Equipe Serra Natural</p>
 
-</body>
-</html>
+</div>
+</div>
+</div>
+
+@stop
