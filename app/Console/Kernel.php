22afb0ca60caad72198c6b61abd8bdfdd18041bb
Igor Trindade <igorlucast>
2015-11-09 22:10:40 -0200

    t

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
                      ->dailyAt('10:01');
 
             $schedule->command('email_Microcity')
-                     ->dailyAt('21:10');
+                     ->dailyAt('22:11');
         }
 
     }
