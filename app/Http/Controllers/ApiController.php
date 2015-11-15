2b9ee799c70da6549b3d21916f8b57fd61852360
Igor Trindade <igorlucast>
2015-11-15 00:37:04 -0200

    T

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -46,11 +46,6 @@ class ApiController extends Controller
 
         if(array_key_exists('callback', $_GET)){
 
-            header('Content-Type: text/javascript; charset=utf8');
-            header('Access-Control-Allow-Origin: https://www.admin.serranatural.com/');
-            header('Access-Control-Max-Age: 3628800');
-            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-
             $callback = $_GET['callback'];
             return $callback.'('.$data.');';
 
