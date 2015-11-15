ef9bb5cacf7c5ec550955e5f0074ed1ddd4e31ea
Igor Trindade <igorlucast>
2015-11-15 00:35:49 -0200

    T

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -47,7 +47,7 @@ class ApiController extends Controller
         if(array_key_exists('callback', $_GET)){
 
             header('Content-Type: text/javascript; charset=utf8');
-            header('Access-Control-Allow-Origin: http://www.example.com/');
+            header('Access-Control-Allow-Origin: https://www.admin.serranatural.com/');
             header('Access-Control-Max-Age: 3628800');
             header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
