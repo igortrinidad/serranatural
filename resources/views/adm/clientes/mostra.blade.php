d7d7763137640fec71389837e725cc600a474d70
Igor Trindade <igorlucast>
2016-02-20 17:35:41 -0200

    T

--- resources/views/adm/clientes/mostra.blade.php
+++ resources/views/adm/clientes/mostra.blade.php
@@ -11,8 +11,7 @@
 	@endif
 
 <div class="row">
-	<div class="col-md-6">
-
+	<div class="col-md-9">
 		<div class="panel panel-default">
 			<div class="panel-heading"><h5>Dados</h5></div>
 			<div class="panel-body">
@@ -33,25 +32,31 @@
 				<p>{{$cliente->email}}</p>
 			</div>
 
-			<div class="form-group">
-				<label>Newsletter</label>
 
-				@if($cliente->opt_email == 1)
-				<a href="/admin/clientes/sairEmail/{{$cliente->id}}"><i class="fa fa-check-square-o"></i></a>
-				@else
-				<a href="/admin/clientes/entrarEmail/{{$cliente->id}}"><i class="fa fa-square-o"></i></a>
-				@endif
-			</div>
-
-			<a href="/admin/clientes/edita/{{$cliente->id}}" class="btn btn-primary">Editar<i class="fa fa-pencil"></i></a>
 
 			</div>
 		</div>
 	</div>
 
-	<div class="col-md-6">
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading"><h5>Ações</h5></div>
+			<div class="panel-body">
+				<div class="form-group">
+					<label>Newsletter </label>
+					@if($cliente->opt_email == 1)
+					<a href="/admin/clientes/sairEmail/{{$cliente->id}}"><i class="fa fa-check-square-o"></i></a>
+					@else
+					<a href="/admin/clientes/entrarEmail/{{$cliente->id}}"><i class="fa fa-square-o"></i></a>
+					@endif
+				</div>
 
+				<a href="/admin/clientes/edita/{{$cliente->id}}" class="btn btn-primary btn-block">Editar<i class="fa fa-pencil"></i></a>
+
+			</div>
+		</div>
 	</div>
+
 </div>
 
 
