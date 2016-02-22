3cfb2061d36448b1a36966e8eb853a0f4ff9339e
Igor Trindade <igorlucast>
2016-02-22 18:30:17 -0300

    Middleware Controller Marketing

--- app/Http/Controllers/MarketingController.php
+++ app/Http/Controllers/MarketingController.php
@@ -15,6 +15,11 @@ use Flash;
 
 class MarketingController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
