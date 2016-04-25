ab28a105b94f639f4c1ddc121b20dd649676a182
Igor Trindade <igorlucast>
2016-04-25 20:10:17 -0300

    T

--- resources/views/adm/marketing/edita.blade.php
+++ resources/views/adm/marketing/edita.blade.php
@@ -88,7 +88,25 @@
 					  height: 400,                 // set editor height
 					  minHeight: null,             // set minimum height of editor
 					  maxHeight: null,             // set maximum height of editor
-					  focus: true                  // set focus to editable area after initializing summernote
+					  focus: true,
+					  toolbar: [
+    // [groupName, [list of button]]
+					    ['style', ['bold', 'italic', 'underline', 'clear']],
+					    ['font', ['strikethrough', 'superscript', 'subscript']],
+					    ['fontsize', ['fontsize']],
+					    ['color', ['color']],
+					    ['para', ['ul', 'ol', 'paragraph']],
+					    ['height', ['height']],
+					    ['picture', ['picture']],
+					    ['video', ['video']],
+					    ['link', ['link']],
+					    ['table', ['table']],
+					    ['hr', ['hr']],
+					    ['codeview', ['codeview']],
+					    ['undo', ['undo']],
+					    ['redo', ['redo']]
+    				]
+
 					});
 
 			</script>
