07058f0d9ca9b2bde9a67aa265a6760813ecdbc1
igortrinidad <igorlucast@hotmail.com>
2016-10-16 15:11:30 -0200

    Added cardapio

--- resources/views/landing/cardapio.blade.php
+++ resources/views/landing/cardapio.blade.php
@@ -4,6 +4,9 @@
 
 	@include('errors.messages')
 
+    <div id="elCardapio">
+        
+
         @if($pratoDeHoje)
 	    <div class="row">
             <div class="box">
@@ -53,19 +56,104 @@
                         <strong>Cardápio</strong>
                     </h2>
                     <hr>
+        
+                    <button class="btn btn-default" v-for="menu in sections" @click="interactions.cardapioSelected = menu">@{{menu.name}}</button> 
 
-                    <h1>Em breve</h1>
+                    <br>
+                    <br>
+                    <div v-for="menu in sections" class="row">
+                        <div v-if="interactions.cardapioSelected == menu" transition="fade">
+                            <div class="col-md-6 col-xs-12 ">
+                                <img v-bind:src="menu.url1" width="80%" alt="Serra Natural"/>
+                            </div>
+                            <div class="col-md-6 col-xs-12">
+                                <img :src="menu.url2" width="80%" alt="Serra Natural"/>
+                            </div>
+                        </div>
+                        
+                    </div>
 
                 </div>
             </div>
         </div>
+
+    </div>
 
 
     @section('scripts')
 	    @parent
 	        
 		<script type="text/javascript">
+                Vue.config.debug = true;
+                Vue.http.headers.common['X-CSRF-TOKEN'] = $('input[name="_token"]').val();
+                var vm = new Vue({
+                    el: '#elCardapio',
+                    data: 
+                    {
+                        interactions: {
+                            cardapioSelected: {}
+                        },
+                        sections: [
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_2.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_3.png',
+                                name: 'Açaí'
+                            },
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_4.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_5.png',
+                                name: 'Salada de Frutas'
+                            },
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_6.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_7.png',
+                                name: 'Sucos especiais'
+                            },
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_8.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_9.png',
+                                name: 'Sucos montados'
+                            },
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_10.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_11.png',
+                                name: 'Café e comidinhas'
+                            },
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_12.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_13.png',
+                                name: 'Smoothies'
+                            },
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_14.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_15.png',
+                                name: 'Saladas'
+                            },
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_16.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_17.png',
+                                name: 'Drinks'
+                            },
+                            {
+                                url1: '/landing/cardapio/out-2016/CARDAPIO_21.png',
+                                url2: '/landing/cardapio/out-2016/CARDAPIO_20.png',
+                                name: 'Sandwichs'
+                            }
+                        ]
+                    },
+                    ready: function(){
+                        var that = this
 
+                        that.interactions.cardapioSelected = that.sections[0];
+                    },
+                    methods: {
+                        goCliente: function(ev){
+                            ev.preventDefault()
+
+                            window.location.href = '/cliente/' + this.emailCliente;
+                        }
+                    }
+                });
 		</script>
 
 	@stop
