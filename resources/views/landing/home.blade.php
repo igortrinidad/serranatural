67739df67fc30d71b780363b49f34baf2b05cb43
Igor Trindade <igorlucast@hotmail.com>
2016-12-09 12:24:40 -0200

    Added google street view

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -4,13 +4,27 @@
 
     <div class="row">
         <div class="box">
+
+            <hr>
+            <h2 class="intro-text text-center">
+                <strong>Faça um tour</strong>
+            </h2>
+            <hr>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m0!3m2!1spt-BR!2sbr!4v1481292965789!6m8!1m7!1sF%3A-jOTxnqmEupc%2FWElm4Onx_zI%2FAAAAAAAAF64%2FarHdjMe58zI9AXdYC-nRnCGu_J7QhLhRgCLIB!2m2!1d-19.98122255332469!2d-43.93946528434753!3f79.6239518401576!4f-2.4485701505543176!5f0.7820865974627469" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        
+    </div>
+
+    <div class="row">
+        <div class="box">
             <div class="col-lg-12 text-center">
                 
                 @include('flash::message')
 
             	<hr>
                 <h2 class="intro-text text-center">
-                    <strong>Bem vindo</strong>
+                    <strong>Fotos</strong>
                 </h2>
                 <hr>
 
